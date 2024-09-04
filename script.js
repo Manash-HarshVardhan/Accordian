21951a39abd765d923dc17d3c09075e2d8d7f015
Manash-HarshVardhan <manash.harshvardhan@axeno.co>
2024-09-04 13:57:24 +0530

    final_commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const accordians=document.querySelectorAll('.accrordian')
 
 accordians.forEach(accor=>{
-    const icon= accor.querySelector('.accrordian__question__icon')
-    const ans=accor.querySelector('.accrordian__answer')
+    const icon= accor.querySelector('.accrordian__firstpart__icon')
+    const ans=accor.querySelector('.accrordian__secpart')
 
   
     accor.addEventListener('click',(e)=>{
         accordians.forEach((single)=>{
             if(accor !== single){
-                const icon= single.querySelector('.accrordian__question__icon')
-                const ans=single.querySelector('.accrordian__answer')
+                const icon= single.querySelector('.accrordian__firstpart__icon')
+                const ans=single.querySelector('.accrordian__secpart')
                 icon.classList.remove('active')
                 ans.style.maxHeight='0';
             }
